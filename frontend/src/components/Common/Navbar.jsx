f9661c5597be70311660b712c8947cfac17ddbe2
Sunil Sharma <sunilpoudel1999@gmail.com>
2025-11-17 00:06:20 +1100

    searchbar-started

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {FaUser,FaCartArrowDown, FaBars} from 'react-icons/fa6'
+import SearchBar from './SearchBar'
 
 const Navbar = () => {
   return (
@@ -9,21 +11,37 @@ const Navbar = () => {
             <Link to="/" className='text-2xl font-medium'>Shoe Corner</Link>
         </div>
         <div className='hidden md:flex space-x-6'>
-            <Link to="#" className='text-gray-700 hover:text-block text-sm font-medium uppercase'>
+            <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
             Addidas
             </Link>
-            <Link to="#" className='text-gray-700 hover:text-block text-sm font-medium uppercase'>
+            <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
             Nike
             </Link>
-            <Link to="#" className='text-gray-700 hover:text-block text-sm font-medium uppercase'>
+            <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
             Jordan
             </Link>
-            <Link to="#" className='text-gray-700 hover:text-block text-sm font-medium uppercase'>
+            <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
             Puma
             </Link>
-            <Link to="#" className='text-gray-700 hover:text-block text-sm font-medium uppercase'>
+            <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
             Skechers
             </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link to="/profile" className='hover:text-black'>
+          <FaUser className='h-6 w-6 text-gray-700' />
+          </Link>
+          <button className='relative hover:text-black'>
+            <FaCartArrowDown className='h-6 w-6 text-gray-700' />
+            <span className='absolute -top-4 -right-1  bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5'>4</span>
+          </button>
+          {/* Search Button */}
+          <div className="overflow-hidden">
+            <SearchBar />
+          </div>
+          <button className='md:hidden'>
+            <FaBars className='h-6 w-6 text-gray-700' />
+          </button>
         </div>
     </nav>
     </>
